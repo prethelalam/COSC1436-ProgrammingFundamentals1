c13ec19b0fcd6e0650a13791b7a13db1b2c79aee
prethelalam <prethelalam@gmail.com>
2022-07-17 19:36:34 -0500

    Made changes to chapter 2 textbook problems

--- chapters/02/textbookProblems.cpp
+++ chapters/02/textbookProblems.cpp
@@ -2,8 +2,6 @@
 #include <iomanip>
 using namespace std;
 
-// Description
-
 int main ()
 {
         cout << std::fixed << std::setprecision(2);
@@ -229,10 +227,6 @@ int main ()
     
     /* 11. Cyborg Data Type Sizes
     You have been given a job as a programmer on a Cyborg supercomputer. In order to accomplish some calculations, you need to know how many bytes the following data types use: char, int, float, and double. You do not have any technical documentation, so you can't look this information up. Write a C++ program that will determine the amount of memory used by these types and display the information on the screen. */
-    cout << "A change goes here" << endl;
-
 
 return 0;
 }
-
-//save file at the end
